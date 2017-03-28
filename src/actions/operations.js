4a8d8f0b7b731f1a1ca58ec821e51b65dafd474d
iankhor <iankhorbc.thecoder@gmail.com>
2017-03-28 13:24:07 +1100

    added counter actions and reducers

---
+++ src/actions/operations.js
@@ -0,0 +1,16 @@
+const ADD = "ADD"
+const SUBTRACT = "SUBTRACT"
+
+export function add(whichCounter){
+  return {
+    type: ADADDD,
+    whichCounter: whichCounter
+  }
+}
+
+export function subtract(){
+ return {
+    type: SUBTRACT,
+    whichCounter: whichCounter
+  }
+}
---
+++ src/reducers/counter.js
@@ -0,0 +1,5 @@
+
+export function counter(state=[], action){
+  console.log(state, action)
+  return state
+}
